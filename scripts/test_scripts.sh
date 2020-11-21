350ec3fa235682225640fc901c8d8d1cf88da667
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-21 20:26:06 +0000

    Create test_scripts.sh

---
+++ scripts/test_scripts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -r CNE-SIFA2/
+git clone "https://github.com/Cpt-Falconator/CNE-SIFA2.git"
+cd CNE-SIFA2/
+mysql -h testing-db.c3up5hij9k9x.eu-west-1.rds.amazonaws.com -P 3306 -u FalconTest -pMWAMDATABASE < database/Create.sql
+docker-compose up -d --build
+docker exec backend bash -c "pytest tests/ --cov application"
+docker exec frontend bash -c "pytest tests/ --cov application"
+docker-compose down
+rm -r CNE-SIFA2/
