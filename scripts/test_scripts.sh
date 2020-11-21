42c0094b7a5e0f9df414ab6ef9cdadf06fd5f80d
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-21 20:47:39 +0000

    Update test_scripts.sh

--- scripts/test_scripts.sh
+++ scripts/test_scripts.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-rm -r CNE-SIFA2/
 git clone "https://github.com/Cpt-Falconator/CNE-SIFA2.git"
 cd CNE-SIFA2/
-mysql -h testing-db.c3up5hij9k9x.eu-west-1.rds.amazonaws.com -P 3306 -u FalconTest -pMWAMDATABASE < database/Create.sql
+mysql -h ${T_DB_END} -P 3306 -u ${T_DB_USERNAME} -p${T_DB_PASSWORD} < database/Create.sql
 docker-compose up -d --build
 docker exec backend bash -c "pytest tests/ --cov application"
 docker exec frontend bash -c "pytest tests/ --cov application"
