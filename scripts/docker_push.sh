2a77965ade0d13546e20891194d81f883c8561ff
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-21 22:03:55 +0000

    Update docker_push.sh

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 docker push cptfalconator/front_end:latest
 docker push cptfalconator/back_end:latest
