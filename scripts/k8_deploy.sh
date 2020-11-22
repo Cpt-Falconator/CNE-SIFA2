7a2b24e0dbbb2b61f9a221478f7be24cb574c751
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-22 14:36:40 +0000

    Update k8_deploy.sh

--- scripts/k8_deploy.sh
+++ scripts/k8_deploy.sh
@@ -5,4 +5,4 @@ sudo kubectl apply -f secrets.yaml
 sudo kubectl apply -f nginx-conf.yaml -n cneproject
 sudo kubectl apply -f load-balancer.yaml -f nginx.yaml -f frontend.yaml -f frontend-cip.yaml -f backend.yaml -f backend-cip.yaml
 sudo kubectl describe service load-balancer -n cneproject
-sudo kubectl describe service nginx -n cneproject
+sudo kubectl describe deployment nginx -n cneproject
