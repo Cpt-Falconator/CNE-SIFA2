a8414161252775a866fc962d853f4e951dc907f7
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-22 16:23:23 +0000

    Update k8_deploy.sh

--- scripts/k8_deploy.sh
+++ scripts/k8_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd K8/
+sudo kubectl delete -f namespace.yaml
+sudo kubectl delete -f secrets.yaml
+sudo kubectl delete -f nginx-conf.yaml -n cneproject
+sudo kubectl delete -f load-balancer.yaml -f nginx.yaml -f frontend.yaml -f frontend-cip.yaml -f backend.yaml -f backend-cip.yaml
 sudo kubectl apply -f namespace.yaml
 sudo kubectl apply -f secrets.yaml
 sudo kubectl apply -f nginx-conf.yaml -n cneproject
