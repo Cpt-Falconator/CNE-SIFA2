828978cd6fbb08df0a24a7df5fe793782907eb06
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-22 17:01:02 +0000

    Update k8_deploy.sh

--- scripts/k8_deploy.sh
+++ scripts/k8_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd K8/
+kubectl apply -f namespace.yaml
 kubectl apply -f secrets.yaml -n cneproject
 kubectl apply -f load-balancer.yaml -f nginx-conf.yaml -f nginx.yaml -n cneproject
 kubectl apply -f frontend-cip.yaml -f frontend.yaml -n cneproject
