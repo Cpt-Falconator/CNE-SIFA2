061d7ca5f73560d088209ffeb963788dd4b1b735
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-22 12:27:09 +0000

    Update and rename k8_deploy.sh to K8/k8_deploy.sh

---
+++ K8/k8_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd K8/
+kubectl apply -f namespace.yaml
+kubectl apply -f secrets.yaml
+kubectl apply -f nginx-conf.yaml
+kubectl apply -f load-balancer.yaml -f nginx.yaml -f frontend-cip.yaml -f frontend.yaml -f backend-cip.yaml -f backend.yaml
+kubectl describe service load-balancer -n cneproject
