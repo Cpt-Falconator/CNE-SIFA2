d7549e291a11122af823a4d699ad36c5f8d480c1
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-23 10:12:16 +0000

    Update test_scripts.sh

--- scripts/test_scripts.sh
+++ scripts/test_scripts.sh
@@ -4,7 +4,6 @@ rm -f CNE-SIFA2/
 git clone "https://github.com/Cpt-Falconator/CNE-SIFA2.git"
 cd /home/ubuntu/CNE-SIFA2/
 mysql -h ${T_DB_END} -P 3306 -u ${T_DB_USERNAME} -p${DB_PASSWORD} < database/Create.sql
-mysql -h ${D_DB_END} -P 3306 -u ${D_DB_USERNAME} -p${DB_PASSWORD} < database/Create.sql
 sudo docker-compose up -d --build
 docker exec backend bash -c "pytest tests/ --cov application"
 docker exec frontend bash -c "pytest tests/ --cov application"
