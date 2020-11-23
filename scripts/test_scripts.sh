547c1cb1fb664b14413bbb57f23cac3d1a8d9483
Lewis Thomson <43699581+Cpt-Falconator@users.noreply.github.com>
2020-11-23 00:31:56 +0000

    Update test_scripts.sh

--- scripts/test_scripts.sh
+++ scripts/test_scripts.sh
@@ -3,7 +3,7 @@
 rm -f CNE-SIFA2/
 git clone "https://github.com/Cpt-Falconator/CNE-SIFA2.git"
 cd /home/ubuntu/CNE-SIFA2/
-mysql -h testing-db.c3up5hij9k9x.eu-west-1.rds.amazonaws.com -P 3306 -u ${T_DB_USERNAME} -p${T_DB_PASSWORD} < database/Create.sql
+mysql -h ${T_DB_END} -P 3306 -u ${T_DB_USERNAME} -p${T_DB_PASSWORD} < database/Create.sql
 mysql -h ${DB_END} -P 3306 -u ${DB_USERNAME} -p${DB_PASSWORD} < database/Create.sql
 sudo docker-compose up -d --build
 docker exec backend bash -c "pytest tests/ --cov application"
